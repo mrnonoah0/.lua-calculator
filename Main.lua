cf798cd64e2449755d4ff7adff1156bd1a05ba63
mrnonoah <noahvellemans@gmail.com>
2025-02-14 22:19:31 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -38,17 +38,14 @@ function InputLoop()
         print("Enter a number")
         local answer = io.read()
         print("Result: " .. math.sin(answer))
-        InputLoop()
     elseif input == "3" then
         print("Enter a number")
         local answer = io.read()
         print("Result: " .. math.cos(answer))
-        InputLoop()
     elseif input == "4" then
         print("Enter a number")
         local answer = io.read()
         print("Result: " .. math.tan(answer))
-        InputLoop()
     elseif input == "5" then
         print("Enter minimum")
         local answer1 = io.read()
@@ -56,8 +53,15 @@ function InputLoop()
         local answer2 = io.read()
     
         print("Result: " .. math.random(answer1, answer2))
-        InputLoop()
     end
+    print()
+    print("[1] = -+/*")
+    print("[2] = sin")
+    print("[3] = cos")
+    print("[4] = tan")
+    print("[5] = Random Number Generator")
+    print()
+    InputLoop()
 end
 
 InputLoop()
